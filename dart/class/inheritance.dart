0cce8ee0fbf6e09201e622bfb3ce483ac393a79a
yassan <84786447+yassan-s@users.noreply.github.com>
2024-05-03 08:07:50 +0900

    Update inheritance.dart

--- dart/class/inheritance.dart
+++ dart/class/inheritance.dart
@@ -4,7 +4,10 @@ class BaseClass {
   void helloWorld() => print("Hello World!");
 }
  
-class SubClass extends BaseClass {}
+class SubClass extends BaseClass {
+  // 既存クラスを拡張して、新しいクラスを作成できる
+  // 継承元のクラスのフィールドとメソッドも使用可能
+}
  
 void main() {
   final subClass = SubClass();
